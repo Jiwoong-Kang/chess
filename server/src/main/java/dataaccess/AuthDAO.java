40ccb0ea84eece2501b4f89ad390cdb4c1c51e3c
Jiwoong <dnd0311@naver.com>
2024-10-14 23:30:58 -0600

    interfaces

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,5 +1,14 @@
 package dataaccess;
 
+import model.AuthData;
 
 public interface AuthDAO {
+
+    void addAuth(AuthData authData);
+
+    void deleteAuth(String authToken);
+
+    AuthData getAuth(String authToken) throws DataAccessException;
+
+    void clear();
 }
