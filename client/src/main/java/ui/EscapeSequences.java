1a51584fb3b2f6dcce49e534e26e4b1c900a74dd
Jiwoong <dnd0311@naver.com>
2024-11-05 17:02:37 -0700

    removed comment out

--- client/src/main/java/ui/EscapeSequences.java
+++ client/src/main/java/ui/EscapeSequences.java
@@ -33,7 +33,6 @@ public class EscapeSequences {
     public static final String SET_TEXT_COLOR_BLUE = SET_TEXT_COLOR + "12m";
     public static final String SET_TEXT_COLOR_MAGENTA = SET_TEXT_COLOR + "5m";
     public static final String SET_TEXT_COLOR_WHITE = SET_TEXT_COLOR + "15m";
-//    public static final String RESET_TEXT_COLOR = UNICODE_ESCAPE + "[39m";
     public static final String RESET_TEXT_COLOR = SET_TEXT_COLOR_WHITE;
 
     public static final String SET_BG_COLOR_BLACK = SET_BG_COLOR + "0m";
@@ -46,7 +45,6 @@ public class EscapeSequences {
     public static final String SET_BG_COLOR_BLUE = SET_BG_COLOR + "12m";
     public static final String SET_BG_COLOR_MAGENTA = SET_BG_COLOR + "5m";
     public static final String SET_BG_COLOR_WHITE = SET_BG_COLOR + "15m";
-//    public static final String RESET_BG_COLOR = UNICODE_ESCAPE + "[49m";
     public static final String RESET_BG_COLOR = SET_BG_COLOR_BLACK;
 
 
