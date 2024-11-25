1c23cb4260eafbce044abfa30be473aff593afea
Jiwoong <dnd0311@naver.com>
2024-11-24 19:33:08 -0700

    serverfacade, websocket working on those

--- client/src/main/java/ui/GameplayREPL.java
+++ client/src/main/java/ui/GameplayREPL.java
@@ -1,24 +1,34 @@
 package ui;
 import chess.ChessGame;
+import chess.ChessMove;
 import chess.ChessPosition;
 import client.ServerFacade;
 import model.GameData;
+import webSocketMessages.serverMessages.Error;
+import webSocketMessages.serverMessages.LoadGame;
+import webSocketMessages.serverMessages.Notification;
+import webSocketMessages.serverMessages.ServerMessage;
+import webSocketMessages.userCommands.MakeMove;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Scanner;
 import static java.lang.System.out;
-import static ui.EscapeSequences.RESET_BG_COLOR;
-import static ui.EscapeSequences.RESET_TEXT_COLOR;
+import static ui.EscapeSequences.*;
 public class GameplayREPL {
+
     ServerFacade server;
-    BoardPrinter boardPrinter;
+    public static BoardPrinter boardPrinter;
     ChessGame game;
-    ChessGame.TeamColor color;
+    int gameID;
+    public static ChessGame.TeamColor color;
+    boolean notificationPending = false;
+
     public GameplayREPL(ServerFacade server, ChessGame game, ChessGame.TeamColor color) {
         this.server = server;
-        this.boardPrinter = new BoardPrinter(game);
-        this.game = game;
+        this.game = gameData.game();
+        this.gameID = gameData.gameID();
         this.color = color;
+        this.boardPrinter = new BoardPrinter(game);
     }
     public void run() {
         boolean inGame = true;
@@ -31,16 +41,17 @@ public class GameplayREPL {
                     printHelpMenu();
                     break;
                 case "redraw":
-                    boardPrinter.printBoard(color, null);
+                    redraw();
                     break;
                 case "leave":
                     inGame = false;
+                    server.leave(gameID);
                     break;
                 case "move":
                     if (input.length == 3 && input[1].matches("[a-h][1-8]") && input[2].matches("[a-h][1-8]")) {
                         ChessPosition from = new ChessPosition(input[1].charAt(1) - '0', input[1].charAt(0) - ('a'-1));
                         ChessPosition to = new ChessPosition(input[2].charAt(1) - '0',input[2].charAt(0) - ('a'-1));
-                        makeMove(from, to);
+                        server.makeMove(gameID, new ChessMove(from, to, null));
                     }
                     else {
                         out.println("Please provide a to and from coordinate (ex: 'c3 d5')");
@@ -48,12 +59,19 @@ public class GameplayREPL {
                     }
                     break;
                 case "resign":
-                    confirmResign();
+                    out.println("Are you sure you want to forfeit? (yes/no)");
+                    String[] confirmation = getUserInput();
+                    if (confirmation.length == 1 && confirmation[0].equalsIgnoreCase("yes")) {
+                        server.resign(gameID);
+                    }
+                    else {
+                        out.println("Resignation cancelled");
+                    }
+
                     break;
                 case "highlight":
                     if (input.length == 2 && input[1].matches("[a-h][1-8]")) {
                         ChessPosition position = new ChessPosition(input[1].charAt(1) - '0', input[1].charAt(0) - ('a'-1));
-                        out.println(position.toString());
                         boardPrinter.printBoard(color, position);
                     }
                     else {
@@ -71,7 +89,7 @@ public class GameplayREPL {
         postloginREPL.run();
     }
     private String[] getUserInput() {
-        String prompt = color == null ? "OBSERVING" : color == ChessGame.TeamColor.WHITE ? "WHITE" : "BLACK";
+        String prompt = "IN-GAME";
         out.printf("\n[%s] >>> ", prompt);
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine().split(" ");
@@ -89,6 +107,9 @@ public class GameplayREPL {
     }
     private void printHighlight() {
         out.println("highlight <coordinate> - highlight all legal moves for the given piece");
+    }
+    private void redraw() {
+        boardPrinter.printBoard(color, null);
     }
     private void makeMove(ChessPosition from, ChessPosition to) {
     }
